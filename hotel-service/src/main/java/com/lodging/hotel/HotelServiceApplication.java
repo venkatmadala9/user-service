a66a70f32c91bfe275877aaf73862df80ec97b2c
vmadala <vmadala@VMADALA-PC.pkrmsezdc.com>
2021-07-27 14:15:03 +0530

    2707

--- hotel-service/src/main/java/com/lodging/hotel/HotelServiceApplication.java
+++ hotel-service/src/main/java/com/lodging/hotel/HotelServiceApplication.java
@@ -2,9 +2,11 @@ package com.lodging.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HotelServiceApplication {
 
 	public static void main(String[] args) {
